ac41d763db649395ff730113ab38d53707c1ebdf
Elnaz Hashemi Nejad <elnaz.hasheminejad.2003@gmail.com>
2024-03-04 17:10:38 +0330

    fisrt Repository of Elnaz-ATM

---
+++ src/Bank.java
@@ -0,0 +1,13 @@
+
+public class Bank {
+	
+		 public static void main(String[]args) { 
+		 Account Elnaz = new Account ("Elnaz" , "IR511481"); 
+		 Account Mina = new Account ("Mina" , "IR453628"); 
+		 //Hossien.showMenu(); 
+		 Mina.showMenu(); 
+		 }
+		  
+		 
+		}
+
